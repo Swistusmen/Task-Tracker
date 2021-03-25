097115eb4b4a7d2be6442b9b5997a86f68f894a2
Swistusmen <michal76aw@gmail.com>
2021-03-25 20:34:27 +0100

    Finished UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Button from "./Button"
 const Header = (props) => {
     return (
         <header>
-            <Button onClick={props.onAdd} color="green" text="Add"/>
+            <Button onClick={props.onAdd} color={props.showAdd? "grey":"green"} text={props.showAdd?"Close":"Add"}/>
         </header>
     )
 }
